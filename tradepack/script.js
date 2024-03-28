c6877b949ba3eaba5648f564acb3c86272559cb3
David William dos Santos <ksgulosao@gmail.com>
2024-03-28 15:42:51 -0300

    Update index.html

--- tradepack/script.js
+++ tradepack/script.js
@@ -11,12 +11,22 @@ let finalValue;
 
 function calcular() {
     value = distance.options[select.selectedIndex].value;
-    finalValue = (base + (tiles * value)) * demand.value;
+    
 
-    if(check.checked) {
-        valueTxt.innerHTML = Math.floor(finalValue * 1.15);
+    if(demand.value >= 1 && demand.value <= 300) {
+        finalValue = (base + (tiles * value)) * (demand.value / 100);
+        if(check.checked) {
+            valueTxt.style.color = "white";
+            valueTxt.innerHTML = Math.floor(finalValue * 1.15);
+        } else {
+            valueTxt.style.color = "white";
+            valueTxt.innerHTML = Math.floor(finalValue);
+        }
     } else {
-        valueTxt.innerHTML = Math.floor(finalValue);
+        valueTxt.style.color = "red";
+        valueTxt.innerHTML = "Por favor, insira um valor entre 1 e 300."
     }
+
+    
 }
 
