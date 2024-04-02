84bb28500d57130c62abbdf463f36356ccb055ae
David William dos Santos <ksgulosao@gmail.com>
2024-04-02 19:03:41 -0300

    Algumas correções e mudanças

--- tradepack/script.js
+++ tradepack/script.js
@@ -1,7 +1,8 @@
 const distance = document.querySelector("#select");
 const demand = document.querySelector("#demand");
 const check = document.querySelector("#check");
-const valueTxt = document.querySelector(".finalValue")
+const warmode = document.querySelector("#warmode");
+const valueTxt = document.querySelector(".finalValue");
 
 const base = 10000;
 const tiles = 6;
@@ -10,23 +11,23 @@ let value;
 let finalValue;
 
 function calcular() {
-    value = distance.options[select.selectedIndex].value;
-    
+  value = distance.options[select.selectedIndex].value;
 
-    if(demand.value >= 1 && demand.value <= 300) {
-        finalValue = (base + (tiles * value)) * (demand.value / 100);
-        if(check.checked) {
-            valueTxt.style.color = "white";
-            valueTxt.innerHTML = Math.floor(finalValue * 1.15);
-        } else {
-            valueTxt.style.color = "white";
-            valueTxt.innerHTML = Math.floor(finalValue);
-        }
+  if (demand.value >= 1 && demand.value <= 300) {
+    finalValue = (base + tiles * value) * (demand.value / 100);
+
+    if (check.checked && warmode.checked) {
+      valueTxt.style.color = "white";
+      valueTxt.innerHTML = Math.floor(finalValue * 1.25);
+    } else if (check.checked) {
+      valueTxt.style.color = "white";
+      valueTxt.innerHTML = Math.floor(finalValue * 1.15);
+    } else if (warmode.checked) {
+      valueTxt.style.color = "white";
+      valueTxt.innerHTML = Math.floor(finalValue * 1.1);
     } else {
-        valueTxt.style.color = "red";
-        valueTxt.innerHTML = "Por favor, insira um valor entre 1 e 300."
+      valueTxt.style.color = "red";
+      valueTxt.innerHTML = "Por favor, insira um valor entre 1 e 300";
     }
-
-    
+  }
 }
-
